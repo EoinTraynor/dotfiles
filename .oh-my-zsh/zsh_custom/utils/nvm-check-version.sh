3abb726737182a55483ed85617ed1dc3dccef215
Eoin Traynor <eointraynor1@gmail.com>
2019-11-09 02:12:01 +0000

    Install node if not already done so

--- .oh-my-zsh/zsh_custom/utils/nvm-check-version.sh
+++ .oh-my-zsh/zsh_custom/utils/nvm-check-version.sh
@@ -1,8 +1,13 @@
 cd() {
   builtin cd "$@"
   if [ -f .nvmrc ];then
-    {     
-      nvm use
-    }
+    {
+      local nvmrc_node_version=$(nvm version "$(cat .nvmrc)")
+      if [ "$nvmrc_node_version" = "N/A" ]; then
+        nvm install
+      elif [ "$nvmrc_node_version" != "$node_version" ]; then
+        nvm use
+      fi
+    } &> /dev/null
   fi
 }
